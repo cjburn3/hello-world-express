73f0b4834f4c1b04b9fac55774f1f4e0f9a716c3
cjburn3 <156855354+cjburn3@users.noreply.github.com>
2024-08-12 20:01:57 -0400

    updates

---
+++ index.js
@@ -0,0 +1,39 @@
+// step 1: Import Express
+const express = require("express");
+
+// step 2: create an express application
+const app = express();
+
+// step 3: define a port
+const PORT = 4000;
+
+// step 4: create a simple route
+app.get("/", (request, response) => {
+  response.send("Hello, world!");
+});
+
+// step 5: Make the server listen on our port
+app.listen(PORT, () => {
+  console.log(`The helloworld server is running on http://localhost:${PORT}`);
+});
+
+// Define other routes we may need
+// GET //read//
+app.get("/hello", (request, response) => {
+  response.send("Hello route GET request");
+});
+
+// POST //create//
+app.post("/hello", (request, response) => {
+  response.send("Hello route POST request");
+});
+Node.js 
+// PUT //update//
+app.put("/hello", (request, response) => {
+  response.send("Hello route PUT request");
+});
+
+// DELETE //delete//
+app.delete("/hello", (request, response) => {
+  response.send("Hello route DELETE request");
+});
